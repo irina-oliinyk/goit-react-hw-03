fd83c2a2306422c538a1951459a4076bc80d58f6
Iryna Oliinyk <arairishka2@gmail.com>
2024-08-23 14:24:44 +0200

    task-2+task-3

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,17 @@ import ContactList from './components/ContactList/ContactList';
 
 function App() {
   const [contact, setContact] = useState(initialContscts);
+  const [filter, setFilter] = useState('');
+  const filterTasck = contact.filter(contact =>
+    contact.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase())
+  );
+
   return (
     <div>
       <h1>Phonebook</h1>
       <ContactForm />
-      <SearchBox />
-      <ContactList contacts={contact} />
+      <SearchBox value={filter} onFilter={setFilter} />
+      <ContactList contacts={filterTasck} />
     </div>
   );
 }
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,3 +1,15 @@
-export default function SearchBox() {
-  return <></>;
+import css from './SearchBox.module.css';
+
+export default function SearchBox({ value, onFilter }) {
+  return (
+    <div className={css.container}>
+      <p className={css.paragraf}>Find contacts by name </p>
+      <input
+        className={css.input}
+        type="text"
+        value={value}
+        onChange={e => onFilter(e.target.value)}
+      />
+    </div>
+  );
 }
